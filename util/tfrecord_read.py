c307a187dc4ec365407e9661fbfe4253235ae562
liuqiucheng <qiucheng.liu@ulsee.com>
2018-06-19 18:08:37 +0800

    1.训练代码 2.landmak数据集处理

---
+++ util/tfrecord_read.py
@@ -0,0 +1,48 @@
+#coding:utf-8
+import tensorflow as tf
+import numpy as np
+import cv2
+import os
+
+# for PNet
+def read_single_tfrecord(tfrecord_file, batch_size, net):
+    # generate a input queue
+    # each epoch shuffle
+    filename_queue = tf.train.string_input_producer([tfrecord_file],shuffle=True)
+    # read tfrecord
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)
+    image_features = tf.parse_single_example(
+        serialized_example,
+        features={
+            'image/encoded': tf.FixedLenFeature([], tf.string),#one image  one record
+            'image/label': tf.FixedLenFeature([], tf.int64),
+            'image/roi': tf.FixedLenFeature([4], tf.float32),
+            'image/landmark': tf.FixedLenFeature([10],tf.float32)
+        }
+    )
+    if net == 'pnet':
+        image_size = 12
+    elif net == 'rnet':
+        image_size = 24
+    elif net == 'onet':
+        image_size = 48
+    else:
+        raise Exception("Unsupport your net type!")
+    image = tf.decode_raw(image_features['image/encoded'], tf.uint8)
+    image = tf.reshape(image, [image_size, image_size, 3])
+    image = (tf.cast(image, tf.float32)-127.5) / 128
+
+    label = tf.cast(image_features['image/label'], tf.float32)
+    roi = tf.cast(image_features['image/roi'],tf.float32)
+    landmark = tf.cast(image_features['image/landmark'],tf.float32)
+    image, label,roi,landmark = tf.train.batch(
+        [image, label,roi,landmark],
+        batch_size=batch_size,
+        num_threads=2,
+        capacity=1 * batch_size
+    )
+    label = tf.reshape(label, [batch_size])
+    roi = tf.reshape(roi,[batch_size,4])
+    landmark = tf.reshape(landmark,[batch_size,10])
+    return image, label, roi,landmark
